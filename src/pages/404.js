dedf64d8a89e6fad8bb57b14e67cd275568a9604
Sharath <sharath.nagaraj@cheproximity.com.au>
2020-04-07 13:15:34 +1000

    404

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,13 +4,10 @@ import { Layout, Hero } from "components"
 
 class FourOhFour extends React.Component {
   render() {
-    const { data } = this.props
-    const siteTitle = data.site.siteMetadata.title
-
     return (
       <Layout location={this.props.location} >
         <div style={{ background: "#fff" }}>
-          <Helmet title={siteTitle} />
+          <Helmet title="404" />
           <Hero data={{
             imageSrc: "home.jpg",
             heading: "404",
